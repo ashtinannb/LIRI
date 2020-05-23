d49246e2d520791656fcf5b51f73b4f8113ae7af
Ashtin Brannon <ashtinannbrannon@gmail.com>
2020-05-22 22:30:11 -0500

    started over after more research

--- liri.js
+++ liri.js
@@ -1,17 +1,27 @@
+// dependencies 
 require("dotenv").config();
 
-// this imports api keys
+// my variables 
 var keys = require("./keys.js");
 
-// axios npm
-var axios = require("axios");
-
-// imports the fs package
-var request = require("fs");
-
-// moment.js npm
-var moment = require('moment');
-
-// inits api client using 
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
+var axios = require("axios");
+var fs = require('fs');
+var moment = require("moment");
+
+var search = process.argv[2];
+
+// Joining the remaining arguments since an actor or tv show name may contain spaces
+var term = process.argv.slice(3).join(" ");
+
+// variable for spotify
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+
+// variable for movies
+var omdbURL = "http://www.omdbapi.com/?t=" + term + "&y=&plot=short&apikey=trilogy";
+
+// variable for concerts
+var bandsURL = "https://rest.bandsintown.com/artists/" + term + "/events?app_id=codingbootcamp";
